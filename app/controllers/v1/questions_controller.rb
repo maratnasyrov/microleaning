895c238897a30ae60e70008cf2c13df36e100890
Marat Nasyrov <marat.online@live.ru>
2017-06-01 22:23:44 +0300

    добавление к выводу всех вопрос, всех ответов

--- app/controllers/v1/questions_controller.rb
+++ app/controllers/v1/questions_controller.rb
@@ -7,11 +7,18 @@ module V1
     skip_before_action :authenticate_user!
 
     def index
-      respond_with json: questions
+      respond_with json: JSON.parse(questions.to_json(
+          :include => { 
+          :answers => { 
+            :only => [ :id, :name, :correct, :question_id ]
+          }},
+        :only => [ :id, :name, :test_id ]
+      ))
     end
 
     def show
       json_out = question.decorate.question_and_answers_json
+      
       respond_with json: json_out
     end
   end
